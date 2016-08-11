8e58347b8eeec364c5799983e7479c2b3e5af276
Roman Malakhov <romaroma@zoom-group.ru>
2016-08-11 20:56:41 +0700

    Cairo installation added

--- base/scripts/lib/install_base.sh
+++ base/scripts/lib/install_base.sh
@@ -2,3 +2,5 @@
 set -e
 apt-get update -y
 apt-get install -y curl bzip2 build-essential python git
+apt-get install -y libcairo2-dev libjpeg62-turbo-dev libpango1.0-dev libgif-dev build-essential g++
+
